2aabd65b98862cacab398304fc61650ea6e02ee7
Indixpert-sumit <sy0050854@gmail.com>
2025-02-27 23:53:37 +0530

    cancel ticket correction

--- Sumit/cancel_ticket.c
+++ Sumit/cancel_ticket.c
@@ -3,17 +3,16 @@ int main()
 {
     int busnumbers[5] ;
 int number;
-     int cancel=0;
+     int cancel=0; 
      int total=50;
     
      printf(" Enter Your Bus Number ");
      scanf("%d",&number);
-    
      printf("\nEnter Numbers Seats Want You Cancel ");
      scanf("%d",&cancel );
 
      printf("\nYour Bus Number:%d ",number);
-     printf("\nYou Cancel Tickets:%d ",cancel);
-printf("\n Total Number Of Seats Remaining%d ",total-cancel );
+     printf("\nYou Canceled Tickets:%d ",cancel);
+printf("\nTotal Number Of Seats Remaining %d ",total-cancel );
 
 }
