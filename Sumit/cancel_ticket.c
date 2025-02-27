009fa93aabfe9e7e1409a3afac615d9937944917
Indixpert-sumit <sy0050854@gmail.com>
2025-02-27 23:48:31 +0530

    cancel ticket

--- Sumit/cancel_ticket.c
+++ Sumit/cancel_ticket.c
@@ -1,1 +1,19 @@
-// test command
+#include<stdio.h>
+int main()
+{
+    int busnumbers[5] ;
+int number;
+     int cancel=0;
+     int total=50;
+    
+     printf(" Enter Your Bus Number ");
+     scanf("%d",&number);
+    
+     printf("\nEnter Numbers Seats Want You Cancel ");
+     scanf("%d",&cancel );
+
+     printf("\nYour Bus Number:%d ",number);
+     printf("\nYou Cancel Tickets:%d ",cancel);
+printf("\n Total Number Of Seats Remaining%d ",total-cancel );
+
+}
