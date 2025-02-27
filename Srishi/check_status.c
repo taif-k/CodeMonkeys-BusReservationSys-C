69a8a19b10be2df648cc8339348f129c3b799e85
srishti <srishti9031htce@gmail.com>
2025-02-27 19:39:22 +0530

    input bus number

--- Srishi/check_status.c
+++ Srishi/check_status.c
@@ -1,1 +1,16 @@
-// test comment 
+//test comment
+
+#include<stdio.h>
+int main(){
+    int num;
+    printf(" please enter Bus Number");
+    scanf("%d", &num);
+    printf("You entered: %d\n" , num);
+
+return 0;
+
+
+}
+
+
+
