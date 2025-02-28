a38de06f3112324d730e4cd5a755f0454ae91906
srishti <srishti9031htce@gmail.com>
2025-02-28 20:23:26 +0530

    handle seat status

--- Srishi/check_status.c
+++ Srishi/check_status.c
@@ -1,16 +1,52 @@
-//test comment
+#include <stdio.h>
+#include <string.h>
 
-#include<stdio.h>
-int main(){
-    int num;
-    printf(" please enter Bus Number");
-    scanf("%d", &num);
-    printf("You entered: %d\n" , num);
+#define TOTAL_SEATS 10  // Total seats in bus
 
-return 0;
+// Structure banaya seat details store karne ke liye
+typedef struct {
+    int seatNo;
+    char name[50];
+    int booked;
+} Seat;
 
+Seat bus[TOTAL_SEATS];
 
+// Bus seats ko initialize karega
+void initializeBus() {
+    for (int i = 0; i < TOTAL_SEATS; i++) {
+        bus[i].seatNo = i + 1;
+        bus[i].booked = 0;  // 0 means seat available
+        strcpy(bus[i].name, "Not Booked");  // Default status
+    }
 }
 
+// Bus ka seat status show karega
+void showStatus() {
+    printf("\nBus Seat Status:\n");
+    printf("Seat No.\tStatus\t\tPassenger Name\n");
+    for (int i = 0; i < TOTAL_SEATS; i++) {
+        printf("%d\t\t%s\t\t%s\n", bus[i].seatNo, 
+               bus[i].booked ? "Booked" : "Available", 
+               bus[i].booked ? bus[i].name : "N/A");
+    }
+}
 
+// Main function
+int main() {
+    initializeBus();  // Bus ko initialize karenge
+    
+    // Kuch seats manually book kar rahe hain (Demo ke liye)
+    bus[2].booked = 1;
+    strcpy(bus[2].name, "taif");
 
+    bus[5].booked = 1;
+    strcpy(bus[5].name, "sumit");
+
+    bus[8].booked = 1;
+    strcpy(bus[8].name, "Ashish");
+
+    showStatus();  // Bus ka seat status show karega
+
+    return 0;
+}
