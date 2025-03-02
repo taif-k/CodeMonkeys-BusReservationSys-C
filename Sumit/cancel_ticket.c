0db24d0d1af8b399605327d304c22c0c83de4abb
Indixpert-sumit <sy0050854@gmail.com>
2025-03-02 21:07:12 +0530

    push for correction

--- Sumit/cancel_ticket.c
+++ Sumit/cancel_ticket.c
@@ -1,18 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int busnumbers[5] ;
-int number;
+    char number;
      int cancel=0; 
      int total=50;
-    
-     printf(" Enter Your Bus Number ");
-     scanf("%d",&number);
-     printf("\nEnter Numbers Seats Want You Cancel ");
-     scanf("%d",&cancel );
+    while(1)
+    {
+      printf(" Enter Your Bus Number ");
 
+     scanf( "%d",&number);
+    if( number==1 )
+     {
+
+        break;
+     }
+     else
+      {
+        printf("! Invalid input !");
+
+      }
+    }
+    
+ printf("\nEnter Number of Seats Want You Cancel ");
+     scanf("%d",&cancel );
      printf("\nYour Bus Number:%d ",number);
      printf("\nYou Canceled Tickets:%d ",cancel);
 printf("\nTotal Number Of Seats Remaining %d ",total-cancel );
+    
 
 }
