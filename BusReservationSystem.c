f0eb2c08fc6cbd3c48e4b2cc4ac77594547eeeaa
taif-k <taifk817@gmail.com>
2025-03-02 12:33:38 +0530

    Write each task in seperate func

--- BusReservationSystem.c
+++ BusReservationSystem.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-void brs();
+void busreservationsys();
 void signup(char uname[10], char password[10]);
 void menu();
 void loginaccount(char username[10], char password[10]);
+void bookticket(int busnumb, int seatbooked, int bus[5][2]);
+void cancelticket(int busnumb, int seatcancelled, int bus[5][2]);
+void checkbusstatus(int busnumb, int bus[5][2]);
 
 int main()
 {
     printf("\n------------------------Bus Reservation System------------------------");
-    brs(); // bus rservation system
+    busreservationsys(); // bus rservation system
 
     return 0;
 }
 
-void brs()
+void busreservationsys()
 {
     int option = 0;
     int count = 0;
-    char userinfo[2][10] = {"cm123", "cm@123"}; // { {"cm123"}, {"cm@123"} }  row = username col = password, 10 is length
     char username[10];
     char password[10];
+    int seatcancel = 0;
     int busnumber = 0;
     int seatbook = 0;
-    int seatcancel = 0;
     int buses[5][2] = {{101, 50}, {102, 50}, {103, 50}, {104, 50}, {105, 50}};
 
     signup(username, password);
@@ -43,140 +45,21 @@ void brs()
             printf("\nEnter User menu task no: ");
             if (scanf("%d", &option) != 1 || option < 1 || option > 4)
             {
-                while (getchar() != '\n')
-                    ;
+                while (getchar() != '\n');
                 continue;
             }
 
             if (option == 1)
             {
-                while (1)
-                {
-                    printf("\nEnter Bus number: ");
-                    if (scanf("%d", &busnumber) != 1)
-                    {
-                        while (getchar() != '\n')
-                            ;
-                        continue;
-                    }
-                    if (busnumber == 101 || busnumber == 102 || busnumber == 103 || busnumber == 104 || busnumber == 105)
-                    {
-                        int match = 0;
-                        for (int i = 0; i < 5; i++)
-                        {
-
-                            if (busnumber == buses[i][0])
-                            {
-                                match = i;
-                                break;
-                            }
-                        }
-
-                        if (match >= 0)
-                        {
-                            printf("\nEnter number of seats to book: ");
-                            scanf("%d", &seatbook); // 2
-
-                            if (seatbook <= buses[match][1]) // 50
-                            {
-                                buses[match][1] = buses[match][1] - seatbook;
-                                printf("\nCongratulations! Seat Booked: %d", seatbook);
-                                printf("\nSeat left %d", buses[match][1]);
-                                break;
-                            }
-                            else
-                            {
-                                printf("\nBooking seats greater than total seats %d", buses[match][1]);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        printf("Enter valid bus Number \n");
-                    }
-                }
+                bookticket(busnumber, seatbook, buses);
             }
             else if (option == 2)
             {
-                while (1)
-                {
-                    printf("\nEnter Bus number: ");
-                    if (scanf("%d", &busnumber) != 1)
-                    {
-                        while (getchar() != '\n')
-                            ;
-                        continue;
-                    }
-                    if (busnumber == 101 || busnumber == 102 || busnumber == 103 || busnumber == 104 || busnumber == 105)
-                    {
-                        int match = 0;
-                        for (int i = 0; i < 5; i++)
-                        {
-
-                            if (busnumber == buses[i][0])
-                            {
-                                match = i;
-                                break;
-                            }
-                        }
-
-                        if (match >= 0)
-                        {
-                            printf("\nEnter number of seats to cancel: ");
-                            scanf("%d", &seatcancel); // 2
-
-                            if (seatbook <= buses[match][1]) // 50
-                            {
-                                buses[match][1] = buses[match][1] + seatcancel;
-                                printf("\nSeats Cancelled: %d", seatcancel);
-                                printf("\nSeats Available: %d", buses[match][1]);
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        printf("\nEnter valid Bus Number ");
-                    }
-                }
+                cancelticket(busnumber, seatcancel, buses);
             }
             else if (option == 3)
             {
-                while (1)
-                {
-                    int fare[5] = {100, 150, 180, 200, 250};
-                    char sourcecity[5][10] = {"Delhi", "Noida", "Gurugram", "Haryana", "Faridabad"};
-                    char destinationcity[5][10] = {"Noida", "Gurugram", "Haryana", "Faridabad", "Agra"};
-
-                    printf("\nEnter Bus Number: ");
-                    if (scanf("%d", &busnumber) != 1) //  101  102..
-                    {
-                        while (getchar() != '\n')
-                            ;
-                        continue;
-                    }
-
-                    int match = -1;
-                    for (int i = 0; i < 5; i++)
-                    {
-                        if (buses[i][0] == busnumber)
-                        {
-                            match = i;
-                            break;
-                        }
-                    }
-
-                    if (match >= 0)
-                    {
-                        printf("\n Bus Number       :       %d", buses[match][0]);
-                        printf("\n Source           :       %s", sourcecity[match]);
-                        printf("\n Destination      :       %s", destinationcity[match]);
-                        printf("\n Total Seats      :       %d", 50);
-                        printf("\n Available Seats  :       %d", buses[match][1]);
-                        printf("\n Fare             :       %d", fare[match]);
-                    }
-                    break;
-                }
+                checkbusstatus(busnumber, buses);
             }
             else if (option == 4)
             {
@@ -192,7 +75,7 @@ void signup(char uname[10], char pword[10])
     while (1)
     {
         printf("\nEnter username to Register: ");
-        scanf("%s", &uname[0]); // Use &uname[0] to pass the address of the first element
+        scanf("%s", &uname[0]);
         int length = strlen(uname);
         int match = 0;
         int numcount = 0;
@@ -211,35 +94,33 @@ void signup(char uname[10], char pword[10])
             }
         }
 
-        if (numcount > 4)
+        if (numcount > 4) // if 4 digits are found in the string, it will update the value of match to 1
         {
             match = 1;
-            printf("\nMore than 4 digits present.\n");
+            printf("\nMore than 4 digits present");
         }
 
-        if (match == 0)
+        if (match == 0) // if digits are not more than 4 and line 35 is not true match is set to 0;
         {
             if (length >= 5)
             {
-                printf("Username is: %s\n", uname);
                 break;
             }
             else
             {
-                printf("\nUsername should be at least 5 characters long.\n");
+                printf("\nUsername should be at least 5 characters long");
             }
         }
     }
 
     while (1)
     {
-        printf("\nEnter password: ");
-        scanf("%s", &pword[0]); // Use &pword[0] to pass the address of the first element
+        printf("Enter password: ");
+        scanf("%s", &pword[0]);
         int length = strlen(pword);
 
-        if (length >= 6)
+        if (length >= 6) // re-ask for password if length is not equal or more than 6
         {
-            printf("Password is : %s\n", pword);
             break;
         }
         else
@@ -251,8 +132,8 @@ void signup(char uname[10], char pword[10])
     while (1)
     {
         char city[10];
-        printf("\nEnter city: ");
-        scanf("%s", &city[0]); // Use &city[0] to pass the address of the first element
+        printf("Enter city: ");
+        scanf("%s", &city[0]);
         int length = strlen(city);
         int match = 0;
 
@@ -269,7 +150,6 @@ void signup(char uname[10], char pword[10])
         {
             if (length >= 5)
             {
-                printf("City is : %s", city);
                 break;
             }
             else
@@ -283,8 +163,8 @@ void signup(char uname[10], char pword[10])
 void loginaccount(char username[10], char password[10])
 {
     int option;
-    char inputUsername[10];
-    char inputPassword[10];
+    char loginusername[10];
+    char loginpassword[10];
 
     do
     {
@@ -302,38 +182,38 @@ void loginaccount(char username[10], char password[10])
             while (1)
             {
                 printf("\nEnter username: ");
-                scanf("%s", &inputUsername[0]); // Use &inputUsername[0] to pass the address of the first element
+                scanf("%s", &loginusername[0]);
                 printf("Enter password: ");
-                scanf("%s", &inputPassword[0]); // Use &inputPassword[0] to pass the address of the first element
+                scanf("%s", &loginpassword[0]);
 
                 int usermatch = 0;
-                for (int i = 0; inputUsername[i] != '\0' || username[i] != '\0'; i++) // Compare each character
+                for (int i = 0; loginusername[i] != '\0' || username[i] != '\0'; i++)
                 {
-                    if (inputUsername[i] != username[i])
+                    if (loginusername[i] != username[i]) // if any char of signup username is not equal to char of login username
                     {
-                        usermatch = 1; // Username mismatch
+                        usermatch = 1;
                         break;
                     }
                 }
 
                 int passwordmatch = 0;
-                for (int i = 0; inputPassword[i] != '\0' || password[i] != '\0'; i++) // Compare each character
+                for (int i = 0; loginpassword[i] != '\0' || password[i] != '\0'; i++)
                 {
-                    if (inputPassword[i] != password[i])
+                    if (loginpassword[i] != password[i]) // if any char of signup password is not equal to char of login password
                     {
-                        passwordmatch = 1; // Password mismatch
+                        passwordmatch = 1;
                         break;
                     }
                 }
 
-                if (usermatch == 0 && passwordmatch == 0)
+                if (usermatch == 0 && passwordmatch == 0) // if both the string exactly match
                 {
-                    printf("\nWelcome, Team Code Monkeys\n");
+                    printf("\nWelcome, Team Code Monkeys");
                     break;
                 }
                 else
                 {
-                    printf("\nEither Username or Password is Incorrect.. Try again\n");
+                    printf("\nEither Username or Password is Incorrect.. Try again");
                 }
             }
         }
@@ -349,4 +229,148 @@ void menu()
 {
     printf("\n1- Login");
     printf("\n2- Exit");
+}
+
+void bookticket(int busnumb, int seatbooked, int bus[5][2])
+{
+    while (1)
+    {
+        printf("\nEnter Bus number: ");
+        if (scanf("%d", &busnumb) != 1)
+        {
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+        if (busnumb == 101 || busnumb == 102 || busnumb == 103 || busnumb == 104 || busnumb == 105)
+        {
+            int match = 0;
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (busnumb == bus[i][0])
+                {
+                    match = i;
+                    break;
+                }
+            }
+
+            if (match >= 0)
+            {
+                printf("\nEnter number of seats to book: ");
+                scanf("%d", &seatbooked); // 2
+
+                if (seatbooked <= bus[match][1]) // 50
+                {
+                    bus[match][1] = bus[match][1] - seatbooked;
+                    printf("\nCongratulations! Seat Booked: %d", seatbooked);
+                    printf("\nSeat left %d", bus[match][1]);
+                    break;
+                }
+                else
+                {
+                    printf("\nBooking seats greater than total seats %d", bus[match][1]);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("Enter valid bus Number \n");
+        }
+    }
+}
+
+void cancelticket(int busnumb, int seatcancelled, int bus[5][2])
+{
+
+    while (1)
+    {
+        printf("\nEnter Bus number: ");
+        if (scanf("%d", &busnumb) != 1)
+        {
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+        if (busnumb == 101 || busnumb == 102 || busnumb == 103 || busnumb == 104 || busnumb == 105)
+        {
+            int match = 0;
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (busnumb == bus[i][0])
+                {
+                    match = i;
+                    break;
+                }
+            }
+
+            if (match >= 0)
+            {
+                while (1)
+                {
+                    printf("\nEnter number of seats to cancel: ");
+                    scanf("%d", &seatcancelled); // 2
+
+                    if (seatcancelled <= bus[match][1]) // 50
+                    {
+                        bus[match][1] = bus[match][1] + seatcancelled;
+                        printf("\nSeats Cancelled: %d", seatcancelled);
+                        printf("\nSeats Available: %d", bus[match][1]);
+                        break;
+                    }
+                    else
+                    {
+                        printf("\nSeats to cancel are greater than avaiable seats");
+                    }
+                }
+                break;
+            }
+        }
+        else
+        {
+            printf("\nEnter valid Bus Number ");
+        }
+    }
+}
+
+void checkbusstatus(int busnumb, int bus[5][2])
+{
+    while (1)
+                {
+                    int fare[5] = {100, 150, 180, 200, 250};
+                    char sourcecity[5][10] = {"Delhi", "Noida", "Gurugram", "Haryana", "Faridabad"};
+                    char destinationcity[5][10] = {"Noida", "Gurugram", "Haryana", "Faridabad", "Agra"};
+
+                    printf("\nEnter Bus Number: ");
+                    if (scanf("%d", &busnumb) != 1) //  101  102..
+                    {
+                        while (getchar() != '\n')
+                            ;
+                        continue;
+                    }
+
+                    int match = -1;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (bus[i][0] == busnumb)
+                        {
+                            match = i;
+                            break;
+                        }
+                    }
+
+                    if (match >= 0)
+                    {
+                        printf("\n Bus Number       :       %d", bus[match][0]);
+                        printf("\n Source           :       %s", sourcecity[match]);
+                        printf("\n Destination      :       %s", destinationcity[match]);
+                        printf("\n Total Seats      :       %d", 50);
+                        printf("\n Available Seats  :       %d", bus[match][1]);
+                        printf("\n Fare             :       %d", fare[match]);
+                    }
+                    break;
+                }
+
 }
