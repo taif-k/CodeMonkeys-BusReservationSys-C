6c69bdda4ae8be45677110c3c13736c081e87744
taif-k <taifk817@gmail.com>
2025-03-03 07:01:09 +0530

    resolve conflict

--- Sumit/cancel_ticket.c
+++ Sumit/cancel_ticket.c
@@ -1,46 +1,18 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
-    char number;
-     int cancel=0; 
-     int total=50;
-    while(1)
-    {
-      printf(" Enter Your Bus Number ");
-
-     scanf( "%d",&number);
-    if( number==1 )
-     {
-
-        break;
-     }
-     else
-      {
-        printf("! Invalid input !");
-
-      }
-    }
-    
- printf("\nEnter Number of Seats Want You Cancel ");
-     scanf("%d",&cancel );
-     printf("\nYour Bus Number:%d ",number);
-     printf("\nYou Canceled Tickets:%d ",cancel);
-printf("\nTotal Number Of Seats Remaining %d ",total-cancel );
-    
-=======
     int number;
     int cancel = 0;
     int total = 50;
 
     while (1)
     {
-        printf(" Enter Your Bus Number ");
->>>>>>> c56c4caa8b55ee0d27c256a1ce4870996f6bd196
+        printf("\nEnter Your Bus Number ");
 
         if (scanf("%d", &number) != 1) // agar bus number abc kuch esa dalega toh neeche while getchar()  inout jo bhi hai usse clear krdega ...aur continue neeche if ko skip krega aur loop phirse start hoga
         {
-            while (getchar() != '\n'); // clear input (input buffer)
+            while (getchar() != '\n')
+                ; // clear input (input buffer)
             continue;
         }
 
@@ -52,6 +24,11 @@ printf("\nTotal Number Of Seats Remaining %d ",total-cancel );
             printf("\nYour Bus Number:%d ", number);
             printf("\nYou Canceled Tickets:%d ", cancel);
             printf("\nTotal Number Of Seats Remaining %d ", total - cancel);
+            break;
+        }
+        else
+        {
+            printf("\nEnter 101 bus number ");
         }
     }
 }
