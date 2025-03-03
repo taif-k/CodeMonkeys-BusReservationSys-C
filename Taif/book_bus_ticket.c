fb85e2b941b570f42e088c8d5bcf5978fe97df89
taif-k <taifk817@gmail.com>
2025-03-03 19:39:45 +0530

    Update validation

--- Taif/book_bus_ticket.c
+++ Taif/book_bus_ticket.c
@@ -14,26 +14,27 @@ void book_ticket()
 {
     int busnumber = 0;
     int seatbook = 0;
-    int buses[5][2] = {{101, 50}, {102, 60}, {103, 70}, {104, 80}, {105, 90}};   // {bus number, total seats}
+    int buses[5][2] = {{101, 50}, {102, 60}, {103, 70}, {104, 80}, {105, 90}}; // {bus number, total seats}
 
     while (1)
     {
-        printf("\nEnter Bus number: ");
+        printf("\nEnter valid Bus number: ");
         if (scanf("%d", &busnumber) != 1)
         {
-            while (getchar() != '\n');  // input buffer is cleared if anything other than integer is given as inout
+            while (getchar() != '\n')
+                ; // input buffer is cleared if anything other than integer is given as inout
             continue;
         }
 
         if (busnumber == 101 || busnumber == 102 || busnumber == 103 || busnumber == 104 || busnumber == 105)
         {
             int match = 0;
-            for (int i = 0; i < 5; i++)  //  loop over buses[][] to get bus number
+            for (int i = 0; i < 5; i++) //  loop over buses[][] to get bus number
             {
 
-                if (busnumber == buses[i][0])  // buses[i][0] bus number is accessed through indexes
+                if (busnumber == buses[i][0]) // buses[i][0] bus number is accessed through indexes
                 {
-                    match = i;  
+                    match = i;
                     break;
                 }
             }
@@ -50,9 +51,15 @@ void book_ticket()
                         continue;
                     }
 
+                    if (seatbook <= 0)
+                    {
+                        printf("\nSeats cannot be Negative");
+                        continue;
+                    }
+
                     if (seatbook <= buses[match][1]) // 50
                     {
-                        printf("\nTotal Seats %d",buses[match][1]);
+                        printf("\nTotal Seats %d", buses[match][1]);
                         buses[match][1] = buses[match][1] - seatbook;
                         printf("\nSeats Booked %d", seatbook);
                         printf("\nSeatleft %d", buses[match][1]);
@@ -71,9 +78,9 @@ void book_ticket()
 
 void busnumbers()
 {
-    printf("Bus Numbers: 101 ");
-    printf(" 102 ");
-    printf(" 103 ");
-    printf(" 104 ");
-    printf(" 105 ");
+    int buses[5][2] = {{101, 50}, {102, 60}, {103, 70}, {104, 80}, {105, 90}}; // {bus number, total seats}
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nBus Number %d : seats%d", buses[i][0], buses[i][1]);
+    }
 }
