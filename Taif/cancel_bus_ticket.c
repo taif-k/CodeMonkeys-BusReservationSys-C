00de4a9b9459ab6af3820a5a04f1efbadb8acdb5
taif-k <taifk817@gmail.com>
2025-03-03 08:56:25 +0530

    Rename function and add comments

--- Taif/cancel_bus_ticket.c
+++ Taif/cancel_bus_ticket.c
@@ -1,28 +1,29 @@
-#include <stdio.h>
+#include <stdio.h> //Header file
 
-void cancelticket();
+void cancel_bus_ticket(); // Declaration
+void busnumbers();  // bus chart
 
-int main()
+int main() // system defined fucntion
 {
-    cancelticket();
+    cancel_bus_ticket(); // calling
     return 0;
 }
 
-void cancelticket()
+void cancel_bus_ticket() // defination
 {
     int busnumber = 0;
-    int seatbook = 0;
-    int buses[5][2] = {{101, 50}, {102, 50}, {103, 50}, {104, 50}, {105, 50}};
-
+    int seatcancel = 0;
+    int buses[5][2] = {{101, 60}, {102, 70}, {103, 80}, {104, 90}, {105, 100}};
+    busnumbers();  // calling bus chart
     while (1)
     {
         printf("\nEnter Bus number: ");
         if (scanf("%d", &busnumber) != 1)
         {
-            while (getchar() != '\n')
-                ;
+            while (getchar() != '\n');
             continue;
         }
+
         if (busnumber == 101 || busnumber == 102 || busnumber == 103 || busnumber == 104 || busnumber == 105)
         {
             int match = 0;
@@ -38,20 +39,39 @@ void cancelticket()
 
             if (match >= 0)
             {
-                printf("\nEnter number of seats to cancel: ");
-                scanf("%d", &seatbook); // 2
+                while (1)
+                {
+                    printf("\nEnter number of seats to cancel: ");
+                    scanf("%d", &seatcancel); // 2
 
-                if (seatbook <= buses[match][1]) // 50
-                {
-                    buses[match][1] = buses[match][1] - seatbook;
-                    printf("\nSeatleft %d", buses[match][1]);
-                    break;
+                    if (seatcancel <= buses[match][1]) // ex: 2 <= 60
+                    {
+                        printf("\nTotal seats %d", buses[match][1]);
+                        printf("\nSeats cancelled %d", seatcancel);
+                        buses[match][1] = buses[match][1] - seatcancel;
+                        printf("\nSeatleft %d", buses[match][1]);
+                        break;
+                    }
+                    else
+                    {
+                        printf("\nCanceling seats greater than total seats %d", buses[match][1]);
+                    }
                 }
-                else
-                {
-                    printf("\nCanceling seats greater than total seats %d", buses[match][1]);
-                }
+                break;
             }
         }
+        else
+        {
+            printf("Please enter valid bus numbers");
+        }
     }
+}
+
+void busnumbers()
+{
+    printf("Bus Numbers: 101 ");
+    printf(" 102 ");
+    printf(" 103 ");
+    printf(" 104 ");
+    printf(" 105 ");
 }
