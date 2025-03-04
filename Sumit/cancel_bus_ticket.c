ea5d78cf67352519346a6bea79393c9f03186738
Indixpert-sumit <sy0050854@gmail.com>
2025-03-04 09:49:19 +0530

    add comments

--- Sumit/cancel_bus_ticket.c
+++ Sumit/cancel_bus_ticket.c
@@ -1,62 +1,62 @@
-#include <stdio.h>
+#include <stdio.h> //Headerfile
 
 void cancel_bus_ticket();
 
-int main()
+int main() // Function
 {
-    cancel_bus_ticket();
+    cancel_bus_ticket(); // Main function
 }
 
-void cancel_bus_ticket()
+void cancel_bus_ticket() // Function
 {
     int number;
     int cancel = 0;
     int total = 50;
-    while (1)
+    while (1) // While Loop
     {
         printf("\nEnter Your Bus Number ");
 
         if (scanf("%d", &number) != 1) //
         {
-            while (getchar() != '\n')
+            while (getchar() != '\n') 
                 ; // clear input (input buffer)
             continue;
         }
 
-        while (1)
+        while (1)//While Loop
         {
             printf("\nEnter Seats you want to Cancel ");
             if (scanf("%d", &cancel) != 1)
             {
-                while (getchar() != '\n')
+                while (getchar() != '\n') // clear input 
                     ;
                 continue;
             }
-            if (number == 101)
+            if (number == 101) //if condition
             {
                 printf("\nYour Bus Number:%d ", number);
                 printf("\nYou Canceled Tickets:%d ", cancel);
                 printf("\nTotal Number Of Seats Remaining %d ", total - cancel);
                 break;
             }
-            else if (number == 102)
+            else if (number == 102) // esle if condition 
             {
                 printf("\nYour Bus Number:%d ", number);
                 printf("\nYou Canceled Tickets:%d ", cancel);
                 printf("\nTotal Number Of Seats Remaining %d ", total - cancel);
                 break;
             }
-            else if (number == 103)
+            else if (number == 103) // else if condition
             {
                 printf("\nYour Bus Number:%d ", number);
                 printf("\nYou Canceled Tickets:%d ", cancel);
                 printf("\nTotal Number Of Seats Remaining %d ", total - cancel);
                 break;
             }
-            else
+            else // else condition
             {
                 printf("\nEnter valid bus number ");
-            }
+            } 
         }
         break;
     }
