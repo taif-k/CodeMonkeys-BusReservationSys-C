6d55c0b7e76a00fad7a9d836b688a4ec4500e65e
taif-k <taifk817@gmail.com>
2025-03-04 07:11:52 +0530

    update validation

--- Srishti/user_management.c
+++ Srishti/user_management.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 
+void login(); // ye ek declaration hai, ye compiler ko btata hai ki ek login finction exist karta hai, jise main ke ander call kiya ja skta hai
 
-void login();//ye ek declaration hai, ye compiler ko btata hai ki ek login finction exist karta hai, jise main ke ander call kiya ja skta hai
-
-int main() {
+int main()
+{
     // login function call
-    login();// function ko call karta hai taki user se input liya ja ske.
-    return 0;// eska matlab hai program succesfully execute ho gya hai.
+    login();  // function ko call karta hai taki user se input liya ja ske.
+    return 0; // eska matlab hai program succesfully execute ho gya hai.
 }
 
 // login function definition
-void login() { // ye function user se input lega aur usko process krega.
+void login()
+{ // ye function user se input lega aur usko process krega.
     int choice;
-    
-    while (1) { // infinite loop, jab tak valid input na mile
-    printf("Enter 1 for Login\n");
-    printf("Enter 2 for Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
 
-    if (choice == 1) {
-        printf("Login Successful!\n");
-        break; // loop ko terminate krega
+    while (1)
+    { // infinite loop, jab tak valid input na mile
+        printf("Enter 1 for Login\n");
+        printf("Enter 2 for Exit\n");
+        printf("\nEnter your choice: ");
+        if (scanf("%d", &choice) != 1)
+        {
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
 
-    } else if (choice == 2) {
-        printf("Exiting...\n");
-        break;
-    } else {
-        printf("Invalid Choice! Please enter 1 or 2.\n");
-        login();  // dubara se input lene ke liye 
+        if (choice == 1)
+        {
+            printf("\nLogin Successful!\n");
+            break; // loop ko terminate krega
+        }
+        else if (choice == 2)
+        {
+            printf("\nExiting...\n");
+            break;
+        }
+        else
+        {
+            printf("\nInvalid Choice! Please enter 1 or 2.\n");
+            login(); // dubara se input lene ke liye
+        }
     }
-}
 }
