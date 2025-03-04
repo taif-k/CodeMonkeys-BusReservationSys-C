2495332aafc86a7853144daa8f509bc2e6e57ef0
Indixpert-sumit <sy0050854@gmail.com>
2025-03-04 21:01:00 +0530

    bus status

--- Sumit/Check_bus_status.c
+++ Sumit/Check_bus_status.c
@@ -1,1 +1,63 @@
-// test
+#include<stdio.h>
+int main()
+{
+int option;
+while(1)
+{
+printf("\nEnter Your Option ");
+
+if(scanf("%d",&option)!= 1)
+{
+    while (getchar() != '\n')
+    continue;
+}
+
+
+if(option==101)
+{ 
+    printf("Fare= 100");
+    printf("\nSource = Delhi");
+    printf("\nDestination = Gurgaon");
+    printf("\nTotal Seats = 60 ");
+   break;
+}
+else if(option==102)
+{
+    printf("Fare= 100");
+    printf("\nSource = Delhi");
+    printf("\nDestination = Gurgaon");
+    printf("\nTotal Seats = 60 ");
+   break;
+}
+else if(option==103)
+{
+    printf("Fare= 100");
+    printf("\nSource = Delhi");
+    printf("\nDestination = Gurgaon");
+    printf("\nTotal Seats = 60 ");
+   break;
+}
+else if(option==104)
+{
+    printf("Fare= 100");
+    printf("\nSource = Delhi");
+    printf("\nDestination = Gurgaon");
+    printf("\nTotal Seats = 60 ");
+   break;
+}
+else if(option==105)
+{
+    printf("Fare= 100");
+    printf("\nSource = Delhi");
+    printf("\nDestination = Gurgaon");
+    printf("\nTotal Seats = 60 ");
+   break;
+}
+else
+{
+printf("Invalid Bus Number");
+
+}
+
+}
+}
