04d6b79be0e9320ce577b51d8aa1c639d6e632b3
taif-k <taifk817@gmail.com>
2025-03-04 18:20:41 +0530

    Update validation and func

--- Taif/logout_account.c
+++ Taif/logout_account.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
+void logout_account(int opt);
+
 int main()
 {
     int option;
+    logout_account(option);
+    return 0;
+}
+
+void logout_account(int opt)
+{
     while (1)
     {
         printf("\nEnter 1 to logout: ");
-        if (scanf("%d", &option) != 1)
+        if (scanf("%d", &opt) != 1)
         {
-            while (getchar() != '\n');
+            while (getchar() != '\n')
+                ;
             continue;
         }
 
-        if (option == 1)
+        if (opt == 1)
         {
             break;
         }
@@ -21,11 +30,4 @@ int main()
             printf("\nPlease select valid option to logout");
         }
     }
-
-    return 0;
-}
-
-void logout()
-{
-    printf("\nEnter 4 to Logout");
 }
