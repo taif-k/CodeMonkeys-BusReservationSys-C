47af3031ae7fd19a8be98bd16fadfd9cf665c7ca
taif-k <taifk817@gmail.com>
2025-03-06 08:33:37 +0530

    remove comments

--- Taif/user_management.c
+++ Taif/user_management.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void menu(); // Declaration
+void menu(); 
 void loginaccount(char username[10], char password[10]);
-// void signup(char uname[10], char password[10]);
 
-int main() // User defined function
+
+int main() 
 {
     char username[10] = {"cm123"};
     char password[10] = {"cm@123"};
-    // signup(username, password); // calling(has Arguments)
+  
     loginaccount(username, password);
     return 0;
 }
 
-void menu() // Definition
+void menu() 
 {
     printf("\n Login credentials (testing purpose): username - cm123, password - cm@123");
     printf("\n1- Login");
@@ -85,96 +85,3 @@ void loginaccount(char usrname[10], char usrpassword[10])
 
     } while (option != 1);
 }
-
-// void signup(char uname[10], char pword[10])
-// {
-//     while (1)
-//     {
-//         printf("\nEnter username to Register: ");
-//         scanf("%s", &uname[0]);
-//         int length = strlen(uname);
-//         int match = 0;
-//         int numcount = 0;
-
-//         for (int i = 0; uname[i] != '\0'; i++)
-//         {
-//             if ((uname[i] < '0' || uname[i] > '9') && (uname[i] != '@') && (uname[i] < 'a' || uname[i] > 'z'))
-//             {
-//                 match = 1;
-//                 break;
-//             }
-
-//             if (uname[i] >= '0' && uname[i] <= '9')
-//             {
-//                 numcount++;
-//             }
-//         }
-
-//         if (numcount > 4) // if 4 digits are found in the string, it will update the value of match to 1
-//         {
-//             match = 1;
-//             printf("\nMore than 4 digits present");
-//         }
-
-//         if (match == 0) // if digits are not more than 4 and line 35 is not true match is set to 0;
-//         {
-//             if (length >= 5)
-//             {
-//                 printf("Username is: %s", uname);
-//                 break;
-//             }
-//             else
-//             {
-//                 printf("\nUsername should be at least 5 characters long");
-//             }
-//         }
-//     }
-
-//     while (1)
-//     {
-//         printf("\nEnter password: ");
-//         scanf("%s", &pword[0]);
-//         int length = strlen(pword);
-
-//         if (length >= 6) // re-ask for password if length is not equal or more than 6
-//         {
-//             printf("Password is : %s\n", pword);
-//             break;
-//         }
-//         else
-//         {
-//             printf("\nPassword should be at least 6 characters long");
-//         }
-//     }
-
-//     while (1)
-//     {
-//         char city[10];
-//         printf("\nEnter city: ");
-//         scanf("%s", &city[0]);
-//         int length = strlen(city);
-//         int match = 0;
-
-//         for (int i = 0; city[i] != '\0'; i++)
-//         {
-//             if (city[i] < 'a' || city[i] > 'z')
-//             {
-//                 match = 1;
-//                 break;
-//             }
-//         }
-
-//         if (match == 0)
-//         {
-//             if (length >= 5)
-//             {
-//                 printf("City is : %s", city);
-//                 break;
-//             }
-//             else
-//             {
-//                 printf("\nCity should be at least 5 characters long");
-//             }
-//         }
-//     }
-// }
