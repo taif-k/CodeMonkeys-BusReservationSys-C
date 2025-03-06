44e49c84a8bb962fedc139834086f2a9554a54e4
taif-k <taifk817@gmail.com>
2025-03-06 07:10:36 +0530

    Merge from taif in dev

--- Taif/book_bus_ticket.c
+++ Taif/book_bus_ticket.c
@@ -19,59 +19,61 @@ void book_bus_ticket() // user-defined function
     while (1)
     {
         printf("\n\nEnter valid Bus number: ");
-        if (scanf("%d", &busnumber) != 1)
+        if (scanf("%d", &busnumber) != 1 || busnumber == '\n')
         {
             while (getchar() != '\n')
                 ; // input buffer is cleared if anything other than integer is given as input
             continue;
         }
 
-        if (busnumber == 101 || busnumber == 102 || busnumber == 103 || busnumber == 104 || busnumber == 105)
+        int match = -1;
+
+        for (int i = 0; i < 5; i++) //  loop over buses[][] to get bus number
         {
-            int match = 0;
-            for (int i = 0; i < 5; i++) //  loop over buses[][] to get bus number
+
+            if (busnumber == buses[i][0]) // buses[i][0] bus number is accessed through indexes
             {
-
-                if (busnumber == buses[i][0]) // buses[i][0] bus number is accessed through indexes
-                {
-                    match = i;
-                    break;
-                }
-            }
-
-            if (match >= 0)
-            {
-                while (1)
-                {
-                    printf("\nEnter number of seats to book: ");
-                    if (scanf("%d", &seatbook) != 1) // 2
-                    {
-                        while (getchar() != '\n')
-                            ;
-                        continue;
-                    }
-
-                    if (seatbook <= 0) // If seat number is entered in negative, rest if-else are skipped and while loop keeps on running
-                    {
-                        printf("Seats cannot be Negative");
-                        continue;
-                    }
-
-                    if (seatbook <= buses[match][1]) // 50
-                    {
-                        printf("\nTotal Seats %d", buses[match][1]);
-                        buses[match][1] = buses[match][1] - seatbook;
-                        printf("\nSeats Booked %d", seatbook);
-                        printf("\nSeatleft %d", buses[match][1]);
-                        break;
-                    }
-                    else
-                    {
-                        printf("\nBooking seats greater than total seats %d", buses[match][1]);
-                    }
-                }
+                match = i;
                 break;
             }
+        }
+
+        if (match >= 0)
+        {
+            while (1)
+            {
+                printf("\nEnter number of seats to book: ");
+                if (scanf("%d", &seatbook) != 1) // 2
+                {
+                    while (getchar() != '\n')
+                        ;
+                    continue;
+                }
+
+                if (seatbook <= 0) // If seat number is entered in negative, rest if-else are skipped and while loop keeps on running
+                {
+                    printf("Seats cannot be Negative");
+                    continue;
+                }
+
+                if (seatbook <= buses[match][1]) // 50
+                {
+                    printf("\nTotal Seats %d", buses[match][1]);
+                    buses[match][1] = buses[match][1] - seatbook;
+                    printf("\nSeats Booked %d", seatbook);
+                    printf("\nSeatleft %d", buses[match][1]);
+                    break;
+                }
+                else
+                {
+                    printf("\nBooking seats greater than total seats %d", buses[match][1]);
+                }
+            }
+            break;
+        }
+        else
+        {
+            printf("\n Invalid Bus Number ");
         }
     }
 }
