26540168fa41b1baa02f4a75ca2dbcb09e291c4a
taif-k <taifk817@gmail.com>
2025-03-06 07:54:10 +0530

    Remove comment

--- Taif/cancel_bus_ticket.c
+++ Taif/cancel_bus_ticket.c
@@ -32,7 +32,7 @@ void cancel_bus_ticket(int booked[5])
         for (int i = 0; i < 5; i++)
         {
 
-            if (busnumber == buses[i][0]) // if input bus number matches from array bus number
+            if (busnumber == buses[i][0]) // if input bus number matches from array bus number 
             {
                 match = i; // match is updated as the i changes
                 break;
